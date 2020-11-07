62eebba43a2523a87b8e913e3164373f6edbe477
Jules <jules.sab132@outlook.fr>
2020-11-07 15:43:38 +0100

    Add is_read to Notifications

--- src/Entity/Notification.php
+++ src/Entity/Notification.php
@@ -33,6 +33,11 @@ class Notification
      */
     private $text;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_read;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -70,6 +75,18 @@ class Notification
     public function setText(?string $text): self
     {
         $this->text = $text;
+
+        return $this;
+    }
+
+    public function getIsRead(): ?bool
+    {
+        return $this->is_read;
+    }
+
+    public function setIsRead(bool $is_read): self
+    {
+        $this->is_read = $is_read;
 
         return $this;
     }
