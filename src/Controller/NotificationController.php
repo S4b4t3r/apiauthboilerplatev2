8e9d079ffee788cf0fea31c4660986b5a010a9b3
Jules <jules.sab132@outlook.fr>
2021-02-06 11:47:48 +0100

    feat notification mark as read route

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Notification;
 use App\Repository\NotificationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -23,5 +26,22 @@ class NotificationController extends AbstractController
             array_push($data['notifications'], $n->serialize);
         }
         return new JsonResponse(json_encode($data));
+    }
+
+    /**
+     * @Route(methods={"POST"}, name="mark_as_read")
+     * @param Notification $notification
+     */
+    public function markAsReadNotifications(Notification $notification): JsonResponse
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+
+        if ($notification->getUser()->getId() === $user->getId() || !is_null($user->getAdmin()))
+        {
+            $notification->setIsRead(true);
+            $manager->flush();
+        }
+        return new JsonResponse(["Notification marked as read!"], 200);
     }
 }
